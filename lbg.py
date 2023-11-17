743fdaa9f132a705dfd21e6f25a22e2ede1af2af
Hugh <hugh2005@icloud.com>
2023-11-17 11:01:15 +0000

    #9

--- lbg.py
+++ lbg.py
@@ -5,7 +5,7 @@ LBG learning-oriented CRUD-based RESTful API using standard Flask routing
 # import Flask microframework and associated tools
 from flask import Flask, request, jsonify
 from flask_api import status
-
+import os
 # import SQL Alchemy (including ORM - Object-relational Mapper - and its data mapper pattern)
 from models import db, ItemModel
 from sqlalchemy import exc
